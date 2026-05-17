320bf91579ccd33cb4c6290fbbe87a85731aeb0c
Cosmos_light5 <thapa48prakash@gmail.com>
2026-05-17 23:30:09 +0545

    first day practice

---
+++ Initials/Practice_level0.py
@@ -0,0 +1,54 @@
+print("hello world")
+print('hello world')
+
+print('welcome to class')
+
+print(5)
+print("5")
+
+welcome_msg=5
+print(welcome_msg)
+
+x=3
+y=3.14
+z="tom"
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+x="5"
+int (x)
+
+x="5"
+int (x)
+print(int(x))
+
+
+number_str="10"
+number = int(number_str)
+print(number)
+print(type(number))
+
+
+
+a,b,c,d,e,f = 1,2,3,"yellow",5.5, "potter"
+is_student=True
+
+print(a,b,c,d,e,f)
+print()
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print()
+print()
+
+print(is_student)
+
+name, age ,city = ("Alice",30,"New York")
+print(name, age ,city)
+
+
