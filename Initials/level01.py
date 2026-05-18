72f023442b24bf7eac91f04b3f2e575773e032d0
Cosmos_light5 <thapa48prakash@gmail.com>
2026-05-18 14:19:02 +0545

    revision

---
+++ Initials/level01.py
@@ -0,0 +1,31 @@
+s="ram"
+f=None
+p=True
+q=5j
+
+print (s)
+print (p)
+print(type(f))
+
+print(f)
+print(q)
+z = 3 + 4j
+#z = 3i [matra lekhe decimal literal error aauxa]
+# Print the whole complex number
+print("Complex number:", z)
+print(type(z))
+# Print real and imaginary parts separately
+print("Real part:", z.real)
+print("Imaginary part:", z.imag)
+
+
+#num00=int(input(input number ))
+# num2=input("no. please: ")
+""" num3=input("no. please: ")
+x=int(num2)
+y=int(num3)
+
+print("concatinated given numbers ", num2+num3)
+add=x + y
+print(int(add))
+print("The sum of the given input is ", add) """
