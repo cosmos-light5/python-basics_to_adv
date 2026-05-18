1ffcbc6aa2f1393e641d72fffc61fb15b2671039
Cosmos_light5 <thapa48prakash@gmail.com>
2026-05-18 14:56:27 +0545

    use of escape sequence

--- Initials/level02.py
+++ Initials/level02.py
@@ -1,5 +1,10 @@
-name= "Ram"
+""" name= "Ram"
 age=5
 print("My name is {} and I am {} yrs old.".format(name , age))
 print(f"My name is {name} and I am {age} yrs old.") #f-string is a string literal that is prefixed with 'f' or 'F'. It allows you to embed expressions inside string literals, using curly braces {}. The expressions inside the curly braces are evaluated at runtime and then formatted using the __formatt__ing__protocol. This makes it easier to create strings that include variable values or expressions without having to use concatenation or the format() method.
-print("My name is ",name, "and I am", age, "yrs old.")
+print("My name is ",name, "and I am", age, "yrs old.") """
+
+print("Hello \n world")
+print("hello \tworld")
+print("hello \\ world")
+print("\'hello world\'")
