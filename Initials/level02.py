584e8f42d38da87d4398e919289ddffa28c97659
Cosmos_light5 <thapa48prakash@gmail.com>
2026-05-18 15:01:36 +0545

    operators and operands basics

--- Initials/level02.py
+++ Initials/level02.py
@@ -2,9 +2,20 @@
 age=5
 print("My name is {} and I am {} yrs old.".format(name , age))
 print(f"My name is {name} and I am {age} yrs old.") #f-string is a string literal that is prefixed with 'f' or 'F'. It allows you to embed expressions inside string literals, using curly braces {}. The expressions inside the curly braces are evaluated at runtime and then formatted using the __formatt__ing__protocol. This makes it easier to create strings that include variable values or expressions without having to use concatenation or the format() method.
-print("My name is ",name, "and I am", age, "yrs old.") """
+print("My name is ",name, "and I am", age, "yrs old.") 
 
+#escape sequence
 print("Hello \n world")
 print("hello \tworld")
 print("hello \\ world")
-print("\'hello world\'")
+print("\'hello world\'")"""
+
+
+num01=int(input("enter a number: "))
+num02=int(input("enter a number: "))
+add=num01 + num02
+sub= num01 - num02
+div= num01/num02
+mod=num01%num02
+expo=num01**num02
+print("sum: {}\ndiff: {}\nquotient: {}\nremainder: {} \nexponential: {}".format(add, sub, div, mod, expo))
