e97c271da170249388b9ccc3f4c4e4296e9827bf
Cosmos_light5 <thapa48prakash@gmail.com>
2026-05-20 14:30:09 +0545

    using initial, end and incremental parameters

--- Initials/level04.py
+++ Initials/level04.py
@@ -2,15 +2,25 @@
 #for loops
 #for variable in sequence:
 #  code to execute
-for number in [1,2,3,4,5]:
+""" for number in [1,2,3,4,5]:
   print(number)
 print("\n")
 for i in range(5):
-  print(i)
+  print(i) 
 
 #to print numbers from 2 to 20
 for i in range(2,21):
   print(i)
+
+#multiples of 3 from 1 to 30
+for i in range(1,11):
+  i*=3
+  print(i)
+"""
+#OR
+for i in range(3,31,3):
+    print(i)
+
 
 #   while condition:
 #   code to execute while condition is true
