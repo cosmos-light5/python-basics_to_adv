e577d11aa1f5683fc45e9c9d06031fdf4a8fe61c
Cosmos_light5 <thapa48prakash@gmail.com>
2026-05-20 14:25:59 +0545

    for loop

--- Initials/level04.py
+++ Initials/level04.py
@@ -1,13 +1,18 @@
 #intro to loops
 #for loops
 #for variable in sequence:
+#  code to execute
 for number in [1,2,3,4,5]:
   print(number)
 print("\n")
 for i in range(5):
   print(i)
 
+#to print numbers from 2 to 20
+for i in range(2,21):
+  print(i)
 
-  """ while False:
-    condition """
+#   while condition:
+#   code to execute while condition is true
+
   
