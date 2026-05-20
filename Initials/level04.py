ab1c9f56ad27135d8a5a2d7aad3db37ccdff5d1c
Cosmos_light5 <thapa48prakash@gmail.com>
2026-05-20 15:01:03 +0545

    upper and lower case

--- Initials/level04.py
+++ Initials/level04.py
@@ -42,7 +42,7 @@ for i in range(3,31,3):   #3-3 increment gareko xa i.e. multiple of 3
 for i in range(3,31,3): #3-3 increment gareko xa i.e. multiple of 3
   if i!=12:
     print(i,end="a ")
-"""
+
 
 for i in range(3,31,3): #3-3 increment gareko xa i.e. multiple of 3
   if i!=12:
@@ -55,6 +55,37 @@ for i in range(3,31,3): #3-3 increment gareko xa i.e. multiple of 3
 for i in range(3,31,3):
   if i!=12 and i%5 != 0:
     print(i)
+
+
+#to find vowel and consonents (conventional way)
+msg= input("give characters to check: ")
+print()
+count=1
+for char in msg:  
+  if char >= "A"and char<="Z" or char>="a" and char<="z":
+    if char=='A'or char=='a' or char=='E'or char=='e' or char=='I'or char=='i' or char=='O'or char=='o' or char=='U'or char=='u':
+      print(count, end=" ")
+      print(char, ":", end="")
+      print(" Vowels")
+    else:
+      print(count, end=" ")
+      print(char, ":", end="")
+      print(" Consonants")
+  else:
+    print("NOT a character..")
+  count+=1
+print("total characters: ", count-1)
+"""
+
+#upper and lower case conversion
+char="X-Man and GODzilla"
+print(char.lower())
+print(char.upper())
+
+#shorter way to find vowel and consonents
+
+
+  #while loops
 #   while condition:
 #   code to execute while condition is true
 
