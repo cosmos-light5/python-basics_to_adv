f750c732039a76d3f797281906160d2b530e9f50
Cosmos_light5 <thapa48prakash@gmail.com>
2026-05-21 23:59:21 +0545

    practice to print given output

--- Initials/level06.py
+++ Initials/level06.py
@@ -21,3 +21,25 @@ while count<=number:
   if count>=20:
     break
   print(count)
+
+
+  count=0
+for count in range(0,7):
+  count+=1
+  if count==1:
+    print(f"Day {count}: Gym Day")
+  elif count==3 or count==5:
+    print(f"Day {count}: Study Day")
+  elif count==6 or count==7:
+    print(f"Day {count}: Weekend! Rest Day")
+  else:
+    print(f"Day {count}: Work Day")
+
+""" output:
+        Day 1: Gym Day
+        Day 2: Work Day
+        Day 3: Study Day
+        Day 4: Work Day
+        Day 5: Study Day
+        Day 6: Weekend! Rest Day
+        Day 7: Weekend! Rest Day"""
