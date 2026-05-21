246b63b92d162e882c4dd966e8d33c538e15da0c
Cosmos_light5 <thapa48prakash@gmail.com>
2026-05-21 23:52:55 +0545

    practice with while loop

--- Initials/level06.py
+++ Initials/level06.py
@@ -1,7 +1,20 @@
+#practice with for loop, continue and break statements
 num=int(input("enter a number: "))
 count=0
 
 for count in range(0,num):
+  count+=1
+  if count%3 == 0:
+    continue
+  if count>=20:
+    break
+  print(count)
+
+
+#practice with while loop, continue and break statements
+number=int(input("enter a number: "))
+count=0
+while count<=number:
   count+=1
   if count%3 == 0:
     continue
