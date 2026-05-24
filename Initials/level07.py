37712ea00baf0ba280d83d144e77ca0d287c3f92
Cosmos_light5 <thapa48prakash@gmail.com>
2026-05-24 16:58:31 +0545

    printing rows and column count

---
+++ Initials/level07.py
@@ -0,0 +1,16 @@
+#display patterns
+for i in range(1,6):          #1 to 5 samma print garxa
+  for j in range(i,5):        #1 to 4 samma print garxa i.e. inner loop 4 times chale paxi matra outer loop ko 1 loop complete hunxa ra 2nd loop suru hunxa total 5x4=20 times chalxa
+    print("i=",i,"j=",j)
+
+""" output:
+i= 1 j= 1
+i= 1 j= 2
+i= 1 j= 3
+i= 1 j= 4
+i= 2 j= 2
+i= 2 j= 3
+i= 2 j= 4
+i= 3 j= 3
+i= 3 j= 4
+i= 4 j= 4"""
