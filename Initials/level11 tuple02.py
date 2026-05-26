c159b53e2ada6da6c368658aa7da25d92709ca63
Cosmos_light5 <thapa48prakash@gmail.com>
2026-05-27 00:06:49 +0545

    output added

--- Initials/level11 tuple02.py
+++ Initials/level11 tuple02.py
@@ -4,6 +4,10 @@ tuple_conversion=tuple(num)
 indexed_conversion=tuple(enumerate(num))
 print(tuple_conversion)
 print(indexed_conversion)         #(0,1) first ko index ho next chai index ko corresponding value
+""" Output:
+(1, 3, 4, 5, 7, 9, 0)
+((0, 1), (1, 3), (2, 4), (3, 5), (4, 7), (5, 9), (6, 0)) """
+
 
 #sample question
 students = [
@@ -17,7 +21,9 @@ for i, stud in enumerate(students):
   for j, grade in enumerate(stud):
     if grade == "A":
       print(stud)               #yo hard type code ho, yesari nagarne
-
+""" Output:
+('Alice', 20, 'A')
+('Charlie', 21, 'A')"""
 
 
 #1st better way
@@ -30,13 +36,18 @@ students = [
 for stud in students:
   if stud[2] == "A":
     print(stud)
-    
+""" Output:
+('Alice', 20, 'A')
+('Charlie', 21, 'A')"""
+
 
 #2nd but best way
-for name, age, grade in students:
+for name, age, grade in students:           #range or len use garera pani garna sakincha
   if grade=="A":
-    print(students)
-
+    print(name, age, grade)
+""" Output:
+Alice 20 A
+Charlie 21 A"""
 
 """ Why to use tuple?
 -Immutability: Tuples cannot be modified after creation, which can help prevent accidental changes to data.
@@ -49,6 +60,6 @@ for name, age, grade in students:
 name="ram"
 student=(('name',name),('age', 10),('grade', 5))
 print(student)
-
+print(student[0][1])  # Output: ram
 """ Output:
 (('name', 'ram'), ('age', 10), ('grade', 5))"""
