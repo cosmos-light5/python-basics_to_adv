40dbc9aaeab91c0797115a1ef43ca5107b33e533
Cosmos_light5 <thapa48prakash@gmail.com>
2026-05-27 00:32:01 +0545

    obtain unique element

--- Initials/lavel12 set intro.py
+++ Initials/lavel12 set intro.py
@@ -16,4 +16,13 @@ print(empty)
 str=("hello, welcome to nepal..")
 str_set=set("hello, welcome to nepal..")
 print(str)
-print(str_set)
+print(str_set)                  #the set will contain unique characters from the string as string is iterable, and the order of characters in the set may not be the same as in the original string due to the unordered nature of sets.
+""" Output:set()
+hello, welcome to nepal..
+{'p', 't', 'm', ' ', '.', ',', 'o', 'c', 'n', 'h', 'e', 'l', 'w', 'a'} """
+
+
+#to eliminate repetition from list
+num=[1,2,2,2,33,44,44,5,5,6,7,77,77,77,898,0]
+unique=list(set(num))         #list lai set ma convert gareko unique element gain garna
+print(unique)
