813b4c16fbc559c021813dcb9c3d98992c82cf4c
Cosmos_light5 <thapa48prakash@gmail.com>
2026-05-28 00:38:25 +0545

    dictionary intro

---
+++ Initials/level 14 dictionary intro.py
@@ -0,0 +1,19 @@
+#Dictionaries
+""" -An unorderes, mutable collection of key-value pairs 
+Key characteristics:
+    -keys must be unique and immutable (string, number, tuples). values can be of any data-type and can be duplicated
+    -unordered in python<3.7(ordered by insertion since 3.7+)
+    -Mutable (can be changed after creation)
+    -Fast lookups by key (O(1)) average case
+    -syntax:
+            created using {} with key-value pairs
+"""
+empty_dict={}
+print(empty_dict)
+
+
+
+
+
+
+#keys cannot be list or set
