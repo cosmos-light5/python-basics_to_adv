0e94aba2e2240de43133f542c5f707d48cd3c603
Cosmos_light5 <thapa48prakash@gmail.com>
2026-05-27 21:13:17 +0545

    difference_update with it's alternative and multi difference of sets

--- Initials/level13 set operations.py
+++ Initials/level13 set operations.py
@@ -5,7 +5,7 @@
     -|= operator: Alternative syntax for update
  """
 
-set1={1,3,5,7,9,11,12}
+""" set1={1,3,5,7,9,11,12}
 set2={2,4,6,8,0,11,12}
 
 union=set1.union(set2)          #.union() method
@@ -25,7 +25,7 @@ print(upd)                  #incorrect way
 set1.update(set2)       #updated elements will be stored either in assumed variable or in set1 i.e. in set just before .update()
 print(set1)             #Output:{0,1,2,3,4,5,6,7,8,9,0,11,12}  i.e. original set1 lai nai change garne raixa..
 #equivalent method
-set1|=set2              #two sets ma update ra |= operator use gare tesle original set change garne vayeko le operate garda aaune 1st set print garne, but multi-union garda chai variable ma rakhera pani garna milxa
+set1|=set2 #            #two sets ma update ra |= operator use gare tesle original set change garne vayeko le operate garda aaune 1st set print garne, but multi-union garda chai variable ma rakhera pani garna milxa
 print(set1)
 
 #multi-union
@@ -38,7 +38,7 @@ print(multi_union)                  #Output:{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 1
 
 #OR operator
 multi_uni=set1|set2|set3
-print(multi_uni)
+print(multi_uni) """
 
 #Intersection
 """ -intersection(): returns a new set with elements common to both sets
@@ -46,7 +46,7 @@ print(multi_uni)
     -intersection_update(): In-place intersection (modifies the original set)
     -&= operator: Alternative syntax for intersection_update"""
 
-set1={1,3,5,7,9,11,12}
+""" set1={1,3,5,7,9,11,12}
 set2={2,4,6,8,0,11,12}
 
 common=set1.intersection(set2)          #variable ma intersection nai name chahi narakhne because its one of the pre defined key-words ho
@@ -63,7 +63,7 @@ set1.intersection_update(set2)
 print(set1)                         #Output:{11, 12}
 
 #equivalent and-equals method
-set1 &= set2
+set1 &= set2 #
 print(set1)                         #Output:{11, 12}
 
 #multiple intersection
@@ -77,7 +77,7 @@ print(multiple_intersection)                #Output: {11}
 #eqv method
 multi_intsec= set1 & set2 & set3
 print(multi_intsec)                         #Output: {11}
-
+ """
 #set operation: Difference
 """ -difference(): returns a new set with elements in the first set but not in second set
     - -operator: Alternative syntax for difference
@@ -85,7 +85,7 @@ print(multi_intsec)                         #Output: {11}
     - -= operator: Alternative syntax for difference_update
 """
 
-set1={1,3,5,7,9,11,12}
+""" set1={1,3,5,7,9,11,12}
 set2={2,4,6,8,0,11,12}
 
 sub=set1.difference(set2)
@@ -102,7 +102,7 @@ set1.difference_update(set2)
 print(set1)             #Output: {1, 3, 5, 7, 9}
 
 #eqv method
-set1 -= set2
+set1 -= set2 #
 print(set1)             #Output: {1, 3, 5, 7, 9}
 
 
@@ -114,5 +114,24 @@ set3={11,8,9,13,24}
 set1.difference_update(set2,set3)
 print(set1)             #output: {1, 3, 5, 7}
 
-
+ """
 #Symmetric difference
+""" -symmetric_difference(): returns element in either set, but not in both
+    -^ operator: alternative syntax for symmetric_difference
+    -symmetric_difference_update(): In-place operation
+    -^= operator: Alternative syntax for symmetric_difference_update
+"""
+
+set1={1,3,5,7,9,11,12}
+set2={2,4,6,8,0,11,12}
+
+sym_diff=set1.symmetric_difference(set2)
+print(sym_diff)
+
+
+set1^=(set2)    #^-->carate symbol; ^ not power operator but ** is.
+print(set1)
+
+
+
+#frozenset
