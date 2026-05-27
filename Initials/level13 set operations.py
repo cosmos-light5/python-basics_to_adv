a87732a81fac8c27fdd42124559ab78a0c527c51
Cosmos_light5 <thapa48prakash@gmail.com>
2026-05-27 18:17:27 +0545

    intersection and it's alternative

--- Initials/level13 set operations.py
+++ Initials/level13 set operations.py
@@ -11,7 +11,7 @@ set2={2,4,6,8,0,11,12}
 union=set1.union(set2)          #.union() method
 print(union)
 #equivalent method
-uni=set1|set2     #| operator for alternative of union;   '| --> pipe'
+uni=set1|set2     #| operator for alternative of union;   '| --> or'
 print(uni)
 
 
@@ -25,7 +25,7 @@ print(upd)                  #incorrect way
 set1.update(set2)       #updated elements will be stored either in assumed variable or in set1 i.e. in set just before .update()
 print(set1)             #Output:{0,1,2,3,4,5,6,7,8,9,0,11,12}  i.e. original set1 lai nai change garne raixa..
 #equivalent method
-set1|=set2              #update ra |= operator le original set change garne vayeko le operate garda aaune 1st set print garne
+set1|=set2              #two sets ma update ra |= operator use gare tesle original set change garne vayeko le operate garda aaune 1st set print garne, but multi-union garda chai variable ma rakhera pani garna milxa
 print(set1)
 
 #multi-union
@@ -35,3 +35,22 @@ set3={2,4,5,7}
 
 multi_union=set1.union(set2,set3)
 print(multi_union)                  #Output:{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12}
+
+#OR operator
+multi_uni=set1|set2|set3
+print(multi_uni)
+
+#Intersection
+""" -intersection(): returns a new set with elements common to both sets
+    -& operator(): Alternative syntax for intersection
+    -intersection_update(): In-place intersection (modifies the original set)
+    -&= operator(): Alternative syntax for intersection_update"""
+
+set1={1,3,5,7,9,11,12}
+set2={2,4,6,8,0,11,12}
+
+common=set1.intersection(set2)          #variable ma intersection nai name chahi narakhne because its one of the pre defined key-words ho
+print(common)
+
+common=set1 & set2
+print(common)
