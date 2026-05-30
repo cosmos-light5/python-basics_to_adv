898f47a38977f6be7acc366a35f01a46522cda01
Cosmos_light5 <thapa48prakash@gmail.com>
2026-05-30 11:52:29 +0545

    append using update

--- Initials/level 15 nested dict.py
+++ Initials/level 15 nested dict.py
@@ -20,19 +20,57 @@ books={"Harry Potter":{"ISBN": 12,
 
 print(books["Harry Potter"]["author"])          #key ko through access
 
-
-books["The Ulchemist"]={"ISBN": 56,
+#new nested book add gareko
+books["The Ulchemist"]={"ISBN": 56,                     
                     "author": "Paollo Kohello",
                     "publish_date": 1994}
 print(books)
 
-print("before update: ", books)
-books.update({"The Ulchemist":{"ISBN": 56,                  #small changes inside the ulchemist dict...
+
+#modifying nested values
+books["The Ulchemist"]["publish_date"]=1995             #small changes inside the ulchemist dict...
+print(books["The Ulchemist"])
+
+
+
+#update() method modifies a dictionary in-place by adding new key-value pairs or overwriting the values of existing keys.
+books={"Harry Potter":{"ISBN": 12,
+                    "author": "J. K. Rollins",
+                    "publish_date": 1990}, 
+
+        "Romeo-Juliet":{"ISBN": 23,
+                    "author": "william shakespears",
+                    "publish_date": 1890}, 
+
+        "Serve-well":{"ISBN": 34,
+                    "author": "Thomas Dritz",
+                    "publish_date": 1987},
+
+        "The art of war":{"ISBN": 45,
+                    "author": "sun Zhu",
+                    "publish_date": 1840},
+
+        "The law of Human Nature":{"ISBN": 56,
+                    "author": "Robert Greene",
+                    "publish_date": 1976}}
+
+new_books = {"The art of war":{"ISBN": 45,
+                    "author": "sun Zhu",
+                    "publish_date": 1845},
+             "Summer love":{"ISBN": 45,
+                    "author": "Subin Bhattarai",
+                    "publish_date": 2020}}
+
+books.update(new_books)         #When you pass another dictionary, existing keys get overwritten, and new keys are appended
+print(books)
+#Or  for quick, pass direct key-value assignments inside the function
+books.update({"The Ulchemist":{"ISBN": 56,                  
                     "author": "Paollo Kohelo",
                     "publish_date": 1997}})
-print("after update: ", books) 
+print(books) 
 
 
+#jaha key vetidaina tya ha error nadiyi comment print garna .get() ko use gare ko..
 print(books.get("Serve-well", "doesnot exists").get("price", "Invalid input.."))
 print(books.get("Serve-well", {}).get("price", "Invalid input.."))
 
