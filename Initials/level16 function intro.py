80a843abe10f277f9b1bd8ba8f325581d240cee2
Cosmos_light5 <thapa48prakash@gmail.com>
2026-06-01 00:18:16 +0545

    displaying the given output with if-else condition

--- Initials/level16 function intro.py
+++ Initials/level16 function intro.py
@@ -55,9 +55,6 @@ print(message)                      #print() is a perform a task type function a
 
 """ # all functions by default returns null """
 
-
-
-
 #Returning Multiple values
 def calculate(x,y):
     sum=x+y
@@ -72,6 +69,35 @@ print(f"add: {sum}, sub:{diff}")                        #sum, diff, product, quo
 result=calculate(10,5)
 print(result)               #multiple print garna ko lagi ra yesle tuple ko form ma dinxa
 
+
+
+#diaplay as given output
+num=int(input("Enter a number: "))
+
+def fizz_Buzz(num):
+  if num%3 == 0 and num%5 == 0:         #if-else use gare 1st condition check garera next condition ma janxa nai tara only if condition use gare ma 1st ko check garxa ra correct vaye tehi stop garxa navaye matera next if conditon ma shift hunxa
+    print("Fizz_Buzz")
+  elif num%5 ==0:
+    print("Buzz")
+  elif num%3 == 0:
+    print("Fizz")
+  else:
+    print(num)
+
+fizz_Buzz(num)                      #function lai call garna nabirsine
+
+#or
+def fizz_Buzz(num):
+  if num%5 ==0:
+    print("Buzz")
+  if num%3 == 0:
+    print("Fizz")
+  if num%3 != 0 and num%5 != 0:         #if-else use gare 1st condition check garera next condition ma janxa nai tara only if condition use gare ma 1st ko check garxa ra correct vaye tehi stop garxa navaye matera next if conditon ma shift hunxa
+    print("num")
+  
+    print(num)
+
+fizz_Buzz(15)  
 
 
 
