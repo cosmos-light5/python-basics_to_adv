f923036597c592b2e6324a27a2ec59e820290f0a
Cosmos_light5 <thapa48prakash@gmail.com>
2026-06-03 22:32:51 +0545

    read_file: interms of def_function

--- File Handling/read.py
+++ File Handling/read.py
@@ -2,3 +2,23 @@ with open("file.txt", "r") as f:
     content = f.read()
     print(content)
 
+
+#in terms of def_function
+def read_file():
+    with open("file.txt", "r") as f:
+        content = f.read()
+        print(content)
+
+read_file()
+
+
+""" 
+#to return the content from the function instead of printing it:
+def read_file():
+    with open("file.txt", "r") as f:
+        return f.read()
+
+content = read_file()
+print(content) 
+
+"""
---
+++ File Handling/update.py
@@ -0,0 +1,5 @@
+#update mode (read-write)--> '+'
+
+with open("file.txt","r+") as f:
+    content=f.read
+    f.write("More Content")
