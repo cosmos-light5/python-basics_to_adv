7c9208678acdfeb056b165779fbbb0ecb9665544
Cosmos_light5 <thapa48prakash@gmail.com>
2026-06-03 22:38:07 +0545

    append_file: interms of def_function

--- File Handling/append.py
+++ File Handling/append.py
@@ -1,2 +1,13 @@
+#append le file exist na vaye new file pani banauxa
 with open("file.txt", "a") as f:
     f.write("Additional content")
+
+
+""" 
+#in terms of def_function
+def append_file():
+    with open("file.txt", "a") as f:
+        f.write("Additional content")
+
+append_file()
+     """
