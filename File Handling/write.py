4c24704ecefabeb4a7071e3c7e411022a9d1ced2
Cosmos_light5 <thapa48prakash@gmail.com>
2026-06-03 22:34:33 +0545

    write_file: interms of def_function

--- File Handling/write.py
+++ File Handling/write.py
@@ -1,3 +1,12 @@
 with open("file.txt", "w") as f:                #f-->pointer ho that points file.txt
     f.write("New content")
     
+""" 
+#in terms of def_function
+def write_file():
+    with open("file.txt", "w") as f:
+        f.write("New content")
+
+write_file()
+     """
+
