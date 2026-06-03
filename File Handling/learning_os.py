8e242a4493df91b8fcbc8856194eeb66f9987e49
Cosmos_light5 <thapa48prakash@gmail.com>
2026-06-03 07:51:21 +0545

    file handling intro

---
+++ File Handling/learning_os.py
@@ -0,0 +1,22 @@
+import os
+print('_____________________________________________________________________')
+print('Current working directory:', os.getcwd())
+
+""" os.chdir(r"\DScience\python-basics_to_adv")
+print("changed working directory: ", end="")
+print(os.getcwd())
+
+#output:
+_____________________________________________________________________
+Current working directory: D:\DScience\python-basics_to_adv
+changed working directory: D:\DScience\python-basics_to_adv
+ """
+
+# print(os.listdir())
+
+# os.mkdir("test_dir")
+# os.rmdir("test_dir")
+
+# os.makedirs("test_dir/sub_dir")
+# os.removedirs("test_dir/sub_dir")
+# os.rename("Copy.jpg", "not_copy.jpg")
