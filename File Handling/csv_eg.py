a3a2d77e255fde9b883c9a44fb65dab6aa84c6dc
Cosmos_light5 <thapa48prakash@gmail.com>
2026-06-07 00:56:51 +0545

    creating csv file and use of read write and append mode to resultout dictionary and list

--- File Handling/csv_eg.py
+++ File Handling/csv_eg.py
@@ -2,7 +2,7 @@ import csv
 
 #writing a csv file
 data=[
-    ["Name", "age", "City"],                #name, age, city--> header ho vane aaru values
+    ["Name", "age", "City"],                #name, age, city--> header ho vane aaru data
     ["Alice", 25, "New York"],
     ["Bob", 30, "Chicago"],
     ["Charlie", 35, "Los Angles"]
@@ -14,11 +14,11 @@ with open("people.csv", "w", newline="") as file:
 
 
 with open("people.csv", "r", newline="") as file:
-    csv_reader=csv.reader(file)
-    header=next(csv_reader)                         #Reads the header row; next le one line print garxa
+    csv_reader=csv.reader(file)                     #csv.reader() reads the file line by line; splits each row into a list using commas as separators.
+    header=next(csv_reader)                         #next() retrieves the next item from an iterator; Since the iterator is currently at the first row, it returns the header row.
     print(f"Header: {header}")
 
-    for row in csv_reader:
+    for row in csv_reader:                          #for loop starts from the first_data_row because next(csv_reader) has already consumed the header row.
         print(row)
 
 
@@ -27,7 +27,7 @@ with open("people.csv", "r", newline="") as file:
     csv_reader=csv.DictReader(file)
   
     for row in csv_reader:
-        print(row)
+        print(row)                          #each row is a dictionary
 
 
 
@@ -42,12 +42,11 @@ with open("people.csv", "r", newline="") as file:
 
 print(students) 
 students.append({"Name": "Prakash", "age": 25, "City":"kathmandu"})
-
 print(students)
 
 
 with open("people-output.csv", mode="w", newline="") as file:
-    fieldnames=["Name","age","City"]
+    fieldnames=["Name","age","City"]                            #yaha variable fieldnames le dictionary ma headers dine kam garxa
     writer=csv.DictWriter(file, fieldnames=fieldnames)
 
     writer.writeheader()
