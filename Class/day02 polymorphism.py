1116f3c90e36de91ae8ed147e33160a38aad3d2c
Cosmos_light5 <thapa48prakash@gmail.com>
2026-06-08 00:49:21 +0545

    operator overloading with +

--- Class/day02 polymorphism.py
+++ Class/day02 polymorphism.py
@@ -23,7 +23,7 @@ for animal in animals:
 #Polymorphism with inheritance(Method Overriding)
 class Employee:
     def work(self):
-        print("Employees work 8 hours.")
+        print("Employees work 8 hours.") 
 
 class Developer(Employee):
     def work(self):
@@ -35,4 +35,25 @@ class Manager(Employee):
 
 workers=[Developer(), Manager()]
 for worker in workers:
-    worker.work()
+    worker.work()               #Employee ko work ma comment hudahudai developer ra manager ko message print vairakhe ko xa on function call
+
+
+#operator overloading
+#__sub__, __add__, __mul__, __truediv__, __lt__, __gt__, __eq__, __init__
+
+
+
+# Operator overloading with +
+class Point:
+    def __init__(self, x, y):
+        self.x=x
+        self.y=y
+    def __add__(self, other):       #overloading the +  operator
+        return Point(self.x + other.x, self.y + other.y)
+    def __str__(self):
+        return f"({self.x},{self.y})"
+    
+p1=Point(4,5)
+p2=Point(2,4)
+p3=p1+p2                    #internally calls p1.__add__(p2)
+print(p3)
